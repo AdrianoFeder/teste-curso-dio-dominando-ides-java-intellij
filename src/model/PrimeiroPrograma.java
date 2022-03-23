75b216e3e5026fd50707480803f6c7ac48749b99
AdrianoFeder <fmotta.adriano@gmail.com>
2022-03-23 20:34:34 -0300

    First commit

---
+++ src/model/PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+package model;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+    Gato gato = new Gato();
+
+    System.out.println(gato);
+
+
+//        int a = 5;
+//        int b = 3;
+//       System.out.println("Hellow World!" + (a+b));
+    }
+
+    }
+
+
+
+
+
+
+
+
